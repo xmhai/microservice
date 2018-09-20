09451af62eeea62ae73601cb7db28d3a3e9c6602
Lin Hai <linhai@crimsonlogic.com>
2018-09-20 18:32:00 +0800

    Initial check-in of Auth-server, eureka-server and edge-server

---
+++ edge-server/src/main/java/sa/tabadul/fasah/microservice/infra/edge/EdgeServerApplication.java
@@ -0,0 +1,20 @@
+package sa.tabadul.fasah.microservice.infra.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableOAuth2Sso
+@SpringBootApplication
+public class EdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServerApplication.class, args);
+	}
+}
