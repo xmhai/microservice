7f5c2069589af5ac49cf8f10d5b39a5543fe38ff
xmhai <xmhai@hotmail.com>
2018-09-28 07:14:25 +0800

    Add notification, template service

---
+++ template-service/src/main/java/com/lin/microservice/service/template/TemplateServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lin.microservice.service.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateServiceApplication.class, args);
+	}
+}
