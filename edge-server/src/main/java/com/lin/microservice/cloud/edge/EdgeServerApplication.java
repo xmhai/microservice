7308ff996feba799310f01e4c95f87eda70ce7bc
xmhai <xmhai@hotmail.com>
2018-09-29 18:52:32 +0800

    Remove Eureka support from Auth server and Edge server
    Not necessary

--- edge-server/src/main/java/com/lin/microservice/cloud/edge/EdgeServerApplication.java
+++ edge-server/src/main/java/com/lin/microservice/cloud/edge/EdgeServerApplication.java
@@ -2,15 +2,9 @@ package com.lin.microservice.cloud.edge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableDiscoveryClient
-@EnableEurekaClient
-//@EnableOAuth2Sso
 @SpringBootApplication
 public class EdgeServerApplication {
 
