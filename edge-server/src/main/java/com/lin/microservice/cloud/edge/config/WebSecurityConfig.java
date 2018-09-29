d18993a95d6b0380fd028444dc86cb692aaa99ae
xmhai <xmhai@hotmail.com>
2018-09-29 18:51:55 +0800

    Solve CORS issue
    8080 (web server) send ajax request to 8081 (edge server)

--- edge-server/src/main/java/com/lin/microservice/cloud/edge/config/WebSecurityConfig.java
+++ edge-server/src/main/java/com/lin/microservice/cloud/edge/config/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.lin.microservice.cloud.edge.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.google.common.collect.ImmutableList;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
 		http.requestMatchers()
-        	.antMatchers("/login", "/oauth/**")
+        	.antMatchers("/login")
 			.and()
 			.authorizeRequests().anyRequest().permitAll()
 			.and()
@@ -17,6 +25,22 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			//.and()
 			//.logout().permitAll()
 			//.and()
-			.csrf().disable();
+			.csrf().disable().cors();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(ImmutableList.of("*"));
+        configuration.setAllowedMethods(ImmutableList.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(ImmutableList.of("Authorization", "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
