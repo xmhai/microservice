527babe9b732a4c31467a9954a2c09709a4dfe69
Lin Hai <linhai@crimsonlogic.com>
2018-10-01 11:40:45 +0800

    Add profile for JBoss deployment

--- eureka-server/src/main/java/com/lin/microservice/cloud/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/lin/microservice/cloud/eureka/EurekaServerApplication.java
@@ -2,13 +2,26 @@ package com.lin.microservice.cloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
+
+
+	// below code are for JBoss deployment
+	
+	// Override the configure method and point your sources to this same class (it’s the entry point of your application). 
+	// Use the passed argument, SpringApplicationBuilder. 
+	// If you don’t do that, your different components won’t be detected, won’t be injected in the context so your application won’t work.
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EurekaServerApplication.class);
+    }
 }
