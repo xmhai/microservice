c9f301887bc254d51658937ff7f3d2ba634e8f61
Lin Hai <linhai@crimsonlogic.com>
2018-10-04 17:42:03 +0800

    Add logback to projects

---
+++ user-service/src/main/java/com/lin/microservice/service/user/ServiceInstanceRestController.java
@@ -0,0 +1,27 @@
+package com.lin.microservice.service.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+class ServiceInstanceRestController {
+	private static Logger logger = LoggerFactory.getLogger(ServiceInstanceRestController.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+    	logger.info("discover client: " + applicationName);
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
--- user-service/src/main/java/com/lin/microservice/service/user/UserServiceApplication.java
+++ user-service/src/main/java/com/lin/microservice/service/user/UserServiceApplication.java
@@ -1,7 +1,11 @@
 package com.lin.microservice.service.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +15,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableDiscoveryClient
 @EnableEurekaClient
 @SpringBootApplication
-public class UserServiceApplication {
+public class UserServiceApplication extends SpringBootServletInitializer {
+	private static Logger logger = LoggerFactory.getLogger(UserServiceApplication.class);
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -19,6 +24,17 @@ public class UserServiceApplication {
 	}
 
 	public static void main(String[] args) {
+		logger.info("*** User Service is starting up...");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
+	
+	// below code are for WAR deployment
+	
+	// Override the configure method and point your sources to this same class (it’s the entry point of your application). 
+	// Use the passed argument, SpringApplicationBuilder. 
+	// If you don’t do that, your different components won’t be detected, won’t be injected in the context so your application won’t work.
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserServiceApplication.class);
+    }
 }
