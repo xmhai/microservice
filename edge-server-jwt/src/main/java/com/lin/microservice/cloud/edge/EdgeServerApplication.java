42e0160335d80b78e726ade65b85790dc22bca22
xmhai <xmhai@hotmail.com>
2018-10-14 19:48:15 +0800

    Separate project for edge server issue jwt for authentication

---
+++ edge-server-jwt/src/main/java/com/lin/microservice/cloud/edge/EdgeServerApplication.java
@@ -0,0 +1,20 @@
+package com.lin.microservice.cloud.edge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class EdgeServerApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EdgeServerApplication.class);
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServerApplication.class, args);
+	}
+}
---
+++ edge-server-jwt/src/main/java/com/lin/microservice/cloud/edge/security/Role.java
@@ -0,0 +1,17 @@
+package com.lin.microservice.cloud.edge.security;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.lin.common.core.BaseEntity;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "idm_role")
+public class Role extends BaseEntity {
+    @Column(name = "role")
+    private String name;
+}
