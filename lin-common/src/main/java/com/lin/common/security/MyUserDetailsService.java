70c902d1f5f4800edf5630db11f13440703bcb30
Lin Hai <linhai@crimsonlogic.com>
2018-10-16 16:59:09 +0800

    Remove MyUserPrincipal class

--- lin-common/src/main/java/com/lin/common/security/MyUserDetailsService.java
+++ lin-common/src/main/java/com/lin/common/security/MyUserDetailsService.java
@@ -1,6 +1,12 @@
 package com.lin.common.security;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,16 +14,37 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
- 
-    @Autowired
-    private UserRepository userRepository;
- 
-    @Override
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
     public UserDetails loadUserByUsername(String username) {
         User user = userRepository.findByUserName(username);
         if (user == null) {
             throw new UsernameNotFoundException(username);
         }
-        return new MyUserPrincipal(user);
+
+        boolean enabled = true;
+		boolean accountNonExpired = true; 
+		boolean credentialsNonExpired = true;
+		boolean accountNonLocked = true;
+        
+        return new org.springframework.security.core.userdetails.User(
+        		user.getUserName(), 
+        		user.getPassword(),
+        		enabled,
+        		accountNonExpired, 
+        		credentialsNonExpired,
+        		accountNonLocked, 
+        		getAuthorities(user.getRoles()));
     }
+
+	private static List<GrantedAuthority> getAuthorities(Set<Role> roles) {
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for (Role role : roles) {
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		}
+		return authorities;
+	}
 }
